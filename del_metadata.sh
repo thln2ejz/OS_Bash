0f21f2c0602332a229e8cc623a8c7b0e8125ab48
thln2ejz <76515490+thln2ejz@users.noreply.github.com>
2021-04-18 23:07:43 +0300

    Update del_metadata.sh

--- del_metadata.sh
+++ del_metadata.sh
@@ -1,59 +1,60 @@
-#!/bin/bash
+#!/usr/bin/env bash
 : '
 Delete docx (Word) file metadata
 
-Usage: ./del_metadata.sh [file_basename].docx 
+Usage:  ./del_metadata.sh file1.docx file2.docx file3.docx ... fileN.docx
 
 ARGS:
-		filename (with docx): Word document , i.e. [file_basename].docx  
+        filenames: Word documents of form [file_basename].docx  
 
-OUTPUT:   
-       
-		file ([file_basename]_cleared.docx) with metadata removed
+OUTPUT:
+
+        files ([file_basename]_cleared.docx) with metadata removed, in current directory
 
 DESCRIPTION:
 
-Deletes metadata (author name and date timestamps) from a docx (Word) 
+Delete metadata (author name and date timestamps) from a docx (Word) 
 document, which is basically a zip file of xml documents. 
-The script modifies document.xml and comments.xml (if present). 
-Metadata in the output file will appear empty (no author), (no date), including
-comments in the margin which will have no author and no timestamps.   
+The script modifies document.xml and comments.xml (if latter is present). 
+Metadata in the output file will appear empty ie (no author), (no date), 
+including comments in the margin which will have no author and no timestamps.
 
-J.A., xvnyjlq@yandex.com
+J.A., xrzfyvqk_k1jw@pm.me
 '
 
-filepath=${@: -1}  #or ${@:$#} or ${!#} or $BASH_ARGV  (last argument)
-mask=$1
+USAGE="Usage: $0 file1.docx file2.docx file3.docx ... fileN.docx"
 
-cp "$filepath" "${filepath%.docx}.zip" 
-filepath=${filepath%.docx}.zip
-
-unzip -l $filepath #list only
-
-unzip "$filepath" "word/document.xml" -d /tmp 
-unzip "$filepath" "word/comments.xml" -d /tmp 
-
-cwd=$(pwd)
-cd /tmp
-
-data=$(cat word/document.xml) #subshell
-data=$(echo $data | sed 's/w:author=\"[^"]*\"/w:author=""/g') 
-data=$(echo $data | sed 's/w:date=\"[^"]*\"/w:date=""/g')
-
-echo $data > word/document.xml
-  
-if [ -f word/comments.xml ]
-then 
-	data=$(cat word/comments.xml)
-	data=$(echo $data | sed 's/w:author=\"[^"]*\"/w:author=""/g') 
-	data=$(echo $data | sed 's/w:date=\"[^"]*\"/w:date=""/g')
-	echo $data > word/comments.xml
+if [ "$#" == "0" ]; then
+    echo "$USAGE"
+    exit 1
 fi
 
-zip --update "$cwd/$filepath" "word/document.xml"
-zip --update "$cwd/$filepath" "word/comments.xml"
+while (( "$#" )); do
+    filepath=$1
+    cp "$filepath" "${filepath%.docx}.zip" 
+    filepath=${filepath%.docx}.zip
 
-rm -fr word 
+    unzip -l $filepath  #list only
+    unzip "$filepath" "word/document.xml" -d /tmp 
+    unzip "$filepath" "word/comments.xml" -d /tmp 
 
-cd "$cwd" 
-mv "$filepath" "${filepath%.zip}_cleared.docx" 
+    cwd=$(pwd)
+    cd /tmp
+
+    data=$(cat word/document.xml | sed -e 's/w:author=\"[^"]*\"/w:author=""/g' -e 's/w:date=\"[^"]*\"/w:date=""/g')
+    echo $data > word/document.xml
+  
+    if [ -f word/comments.xml ]; then
+        data=$(cat word/comments.xml | sed -e 's/w:author=\"[^"]*\"/w:author=""/g' -e 's/w:date=\"[^"]*\"/w:date=""/g')
+        echo $data > word/comments.xml
+    fi
+
+    zip --update "$cwd/$filepath" "word/document.xml"  #second arg _must_ be 'word/document.xml' (ie as is within zip file hierarchy)
+    zip --update "$cwd/$filepath" "word/comments.xml"
+
+    rm -fr ./word
+    cd "$cwd" 
+    mv "$filepath" "${filepath%.zip}_cleared.docx"
+
+    shift
+done
